aaf1daa1bf6a8e2365fbf57cf0335a52e96783b2
Prughvi <prudhviraj3525@gmail.com>
2024-11-07 19:52:53 +0530

    t

---
+++ terraform/main/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  
+}
+resource "aws_instance" "example" {
+  ami   = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "myterraforminstance"
+  }  
+  
+}
+output "instance_ip" {
+  value = aws.instance.example.public_ip  
+  
+}
