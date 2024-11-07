014ed97f258969f67c444b2eeab434e930a9f4d4
Prughvi <prudhviraj3525@gmail.com>
2024-11-07 13:37:20 +0530

    m

--- terraform/new.tf
+++ terraform/new.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "name" {
+resource "aws_instance" "frontend" {
   ami = "ami-09c813fb71547fc4f"
-  vpc_security_group_ids = "sg-0b308c7134616a7ce"
+  vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
   tags = {
     Name = "frontend"
   }
