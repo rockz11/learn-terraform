d8b9eaa6c6f4dbfe1f44b10be3d3c3bf46a01c4c
Prughvi <prudhviraj3525@gmail.com>
2024-12-01 15:54:57 +0530

    m

--- data Sources/main.tf
+++ data Sources/main.tf
@@ -1,17 +1,16 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "test" {
   ami                    = data.aws_ami.ami.id
   instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
   tags = {
-    name = "frontend"
+    name = "test"
   }
 
 
 }
 
 data "aws_ami" "ami" {
-  executable_users = ["self"]
-  most_recent      = true
-  name_regex       = "RHEL-9-DevOps-Practice"
-  owners           = ["973714476881"]
+  most_recent = true
+  name_regex  = "RHEL-9-DevOps-Practice"
+  owners      = ["973714476881"]
 }
