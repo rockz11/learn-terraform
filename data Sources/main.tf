7278fe310c66d58e7f519e4fe4f84f52df8ddb3b
Prughvi <prudhviraj3525@gmail.com>
2024-12-01 16:02:23 +0530

    m

--- data Sources/main.tf
+++ data Sources/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
   ami                    = data.aws_ami.ami.id
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
   tags = {
     name = "test"
   }
@@ -13,4 +13,9 @@ data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "RHEL-9-DevOps-Practice"
   owners      = ["973714476881"]
+}
+
+data "aws_security_group" "sg" {
+  name = "allow-all"
+
 }
