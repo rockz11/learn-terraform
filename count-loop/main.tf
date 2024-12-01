3ea36a090282a7cd23857bf67b0f39fd0f66ea57
Prughvi <prudhviraj3525@gmail.com>
2024-12-01 13:37:24 +0530

    m

--- count-loop/main.tf
+++ count-loop/main.tf
@@ -18,20 +18,22 @@ variable "instances " {
 }
 
 resource "aws_instance" "frontend" {
+  count                  = length(var.instances)
   ami                    = "ami-09c813fb71547fc4f"
   instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
   tags = {
-    name = "frontend"
+    name = var.instances[count.index]
   }
 
 
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "record" {
+  count   = length(var.instances)
   zone_id = "Z00196431INWTJ0O5YT57"
-  name    = "frontend-dev.devops11.online"
+  name    = "$(var.instances[count.index])-dev.devops11.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.instance[count.index].private_ip]
 }
