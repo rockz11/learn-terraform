6c69aca8001e36b0ae95a3afe212ed5d9a026829
Prughvi <prudhviraj3525@gmail.com>
2024-12-01 14:47:26 +0530

    m

---
+++ Roboshop-for-eachloop/main.tf
@@ -0,0 +1,40 @@
+
+variable "instances" {
+  default = {
+    frontend = {}
+    cart = {}
+    mongodb {}
+    cataogue = {}
+    user = {}
+    redis = {}
+    shipping = {}
+    payment = {}
+    rabbitmq = {}
+    dispatch = {}
+    mysql = {}
+
+  }
+  
+
+}
+
+resource "aws_instance" "instance" {
+  for_each              = length(var.instances)
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
+  tags = {
+    name = each.key
+  }
+
+
+}
+
+resource "aws_route53_record" "record" {
+   for_each              = length(var.instances)
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "${each.key}-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance[each.key].private_ip]
+}
