8107484afb32a6a1e463de30e2d49fd6b08340d2
Prughvi <prudhviraj3525@gmail.com>
2024-12-01 17:23:44 +0530

    m

---
+++ 03-Module/ec2/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "instance" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
+  tags = {
+    name = var.instance_name
+
+  }
+
+
+}
+variable "instance_name" {}
---
+++ 03-Module/main.tf
@@ -0,0 +1,20 @@
+variable "instances" {
+  default = [
+    "test1",
+    "test2"
+  ]
+
+}
+
+
+
+module "ec2" {
+  count          = length(var.instances)
+  source         = "./ec2"
+  iinstance_name = var.instances[count.index]
+
+}
+#module "route53" {
+#source = "./reoute53"
+
+#}
