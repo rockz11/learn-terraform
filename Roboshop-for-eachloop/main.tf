817ce9b1290c8b9f06b9bdef9bd5e849984bcc9c
Prughvi <prudhviraj3525@gmail.com>
2024-12-01 14:50:40 +0530

    m

--- Roboshop-for-eachloop/main.tf
+++ Roboshop-for-eachloop/main.tf
@@ -18,7 +18,7 @@ variable "instances" {
 
 }
 
-resource "aws_instance" "instance" {
+resource "aws_instance" "instances" {
   for_each               = length(var.instances)
   ami                    = "ami-09c813fb71547fc4f"
   instance_type          = "t3.small"
