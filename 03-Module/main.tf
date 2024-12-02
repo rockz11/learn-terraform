dd5fc17093791e0b0761eab66533671ffb53610f
Prughvi <prudhviraj3525@gmail.com>
2024-12-02 19:08:34 +0530

    m

--- 03-Module/main.tf
+++ 03-Module/main.tf
@@ -21,6 +21,6 @@ output "ip" {
 module "route53" {
   count         = length(var.instances)
   source        = "./route53"
-  instance_name = var.instanes[count.index]
+  instance_name = var.instances[count.index]
   ip_address    = module.ec2[count.index].ip_address
 }
